e8fc946ea19d552327c5bc486ccbea08a16c9b1d
Tuan Nguyen <quoctuancntv@gmail.com>
2019-05-30 17:11:52 +0700

    first commit

---
+++ mainpage/js/javascript.js
@@ -0,0 +1,19 @@
+// jQuery(function($){
+//     "use strict"
+//     $(document).ready(function(){
+//         $('.op-menu').click(function(){
+//             $(this).toggleClass('clo-menu');
+//         });
+//         $('.btn').each(function(index, value){
+// 			var ThangCha = $(value);
+// 			var TenThangDivTitleMenu = ThangCha.find('.op-menu');
+// 			TenThangDivTitleMenu.click(function(){
+// 				var ThangBiDropdown = ThangCha.find('.menu');
+// 				ThangBiDropdown.slideToggle('fast');
+// 			});
+// 		});
+//     })
+//     $(window).on('load', function(){
+
+// 	});
+// })
